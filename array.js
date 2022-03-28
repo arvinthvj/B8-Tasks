7478bc1d1566c57f52e8d0fee1cc8c938041d24d
Arvinth <arvinth8122@gmail.com>
2022-03-28 08:35:21 +0530

    what is common for all

--- array.js
+++ array.js
@@ -1,57 +1,91 @@
-let data  = [
-    {
-        name : "Sharmila",
-        experience : 5,
-        technologyKnown : ["node", "javascript", "html", "css"],
-        place : "chennai",
-        company : 'TCS'
-    },
-    {
-        name : "Nithesh",
-        experience : 4,
-        technologyKnown : ["javascript", "bootstrap"],
-        place : "tanjore",
-        company : 'TCS'
-    },
-    {
-        name : "Arvinth",
-        experience : 3,
-        technologyKnown : ["javascript","node", "firebase"],
-        place : "vilupuram",
-        company : 'MP'
-    },
-    {
-        name : "Sivakumar",
-        experience : 0.2,
-        technologyKnown : ["javascript"],
-        place : "chennai",
-        company : 'HCL'
-    },
-    {
-        name : "Saravana Kumar",
-        experience : 10,
-        technologyKnown : ["node", "firebase", "javascript", "bootstrap", "oAuth", "jQuery", "React", "Angular"],
-        place : "chennai",
-        company : 'Mind Tree'
-    }
+let data = [
+  {
+    name: "Sharmila",
+    experience: 5,
+    technologyKnown: ["javascript", "html"],
+    place: "chennai",
+    company: "TCS",
+  },
+  {
+    name: "Nithesh",
+    experience: 4,
+    technologyKnown: ["javascript", "bootstrap", "html"],
+    place: "tanjore",
+    company: "TCS",
+  },
+  {
+    name: "Arvinth",
+    experience: 3,
+    technologyKnown: ["javascript", "node", "firebase", "html"],
+    place: "vilupuram",
+    company: "MP",
+  },
+  {
+    name: "Sivakumar",
+    experience: 0.2,
+    technologyKnown: ["node", "javascript", "html", "css"],
+    place: "chennai",
+    company: "HCL",
+  },
+  {
+    name: "Saravana Kumar",
+    experience: 10,
+    technologyKnown: [
+      "node",
+      "firebase",
+      "javascript",
+      "bootstrap",
+      "oAuth",
+      "jQuery",
+      "React",
+      "Angular",
+      "html",
+    ],
+    place: "chennai",
+    company: "Mind Tree",
+  },
 ];
 
 
 
+//get me the list of technologies that have been the same for all ?
 
-// how many people are there in this list ? 
+let whatIsCommon = data.reduce((extraVariable, e, index) => {
+    debugger
+    let toReturnWithoutDuplicates = [];
+    extraVariable = [...extraVariable, ...e.technologyKnown];
 
+    if (index == data.length - 1) {
+      extraVariable.map((m) => {
+        if (!toReturnWithoutDuplicates.includes(m)) {
+          toReturnWithoutDuplicates.push(m);
+        }
+      });
+      return toReturnWithoutDuplicates;
+    }else {
+      return extraVariable;
+    }
+  }, []).reduce((extraVariable1, value, index ,resultFromDuplicatesRemoved) => {
+      debugger
+      let toFinal = [];
+      data.map((l) => {
+        if (l.technologyKnown.includes(value)) {
+          extraVariable1[value] = extraVariable1[value] ? extraVariable1[value] + 1: 1;
+        }
+      });
 
-// how many places are there totally ? 
-
-
-//how many companies are there totally? 
-
-
-//how many technologies have been mentioned for all ?
-
-
-//how many people are there with >= 5 years of experience ?
-
-
-//get me the list of technologies that have been the same for all ? 
+      if (resultFromDuplicatesRemoved.length - 1 == index) {
+        Object.keys(extraVariable1).map((h) => {
+          if (extraVariable1[h] == data.length) {
+            toFinal.push(h);
+          }
+        });
+        return toFinal;
+      } else {
+        return extraVariable1;
+      }
+    },{}
+  );
+  
+  
+  
